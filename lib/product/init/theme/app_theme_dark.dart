be4b26b5a51ec1dd16542fd3dde43abfd183df30
Hakan Peksen <hknpksn@gmail.com>
2022-04-11 10:14:12 +0300

    light ve dark tema da düzenleme yapıldı

--- lib/product/init/theme/app_theme_dark.dart
+++ lib/product/init/theme/app_theme_dark.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AppThemeDark {
-  static AppThemeDark? _instance;
-  static AppThemeDark get instance => _instance ??= AppThemeDark._init();
+class DarkTheme {
+  final _darkColor = _DarkColor();
 
-  AppThemeDark._init();
+  late ThemeData theme;
 
-  static const meadow = Color(0xff19a54a);
-  static const woodsmoke = Color(0xff121515);
-  static const white = Color(0xffffffff); // elavated login text
-  static const gray = Color(0xff888888); // your email vs text
-  static const athensGray = Color(0xfff9fafb); // textfield fillcolor(xx)
-
-  static const scorpion = Color(0xff595959); // search
-  static const shark = Color(0xff1a1e1e); // textfield  textstyle
-
-  static const red = Colors.red;
-
-  ThemeData get themeDataDark => ThemeData(
+  DarkTheme() {
+    theme = ThemeData(
         brightness: Brightness.dark,
         fontFamily: GoogleFonts.karla().fontFamily,
-        textTheme: const TextTheme(
-          subtitle1: TextStyle(color: athensGray, fontWeight: FontWeight.w400), // sign to account
-          subtitle2:
-              TextStyle(color: athensGray, fontWeight: FontWeight.w400), // your email, password
+        textTheme: TextTheme(
+          subtitle1: TextStyle(
+              color: _darkColor.athensGray, fontWeight: FontWeight.w400), // sign to account
+          subtitle2: TextStyle(
+              color: _darkColor.athensGray, fontWeight: FontWeight.w400), // your email, password
           headline5: TextStyle(
               fontWeight: FontWeight.bold,
-              color: white), // Login, Register başlık // have account fs:16
+              color: _darkColor.white), // Login, Register başlık // have account fs:16
           headline6: TextStyle(
               fontWeight: FontWeight.bold,
-              color: white,
+              color: _darkColor.white,
               fontSize: 16), //elevated button Login, Register text
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(60), primary: meadow),
+          style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(60), primary: _darkColor.meadow),
         ),
         colorScheme: ThemeData.dark().colorScheme.copyWith(
-              primary: meadow, // elevated button background, seperated color
-              onPrimary: woodsmoke, // elavated login text,  draggable container color
-              secondary: scorpion,
-              onSecondary: scorpion,
-              error: red,
-              onError: scorpion,
-              surface: white, // textfield  textstyle, SecureIcon(IconThemedata ile)
-              background: athensGray, // textfield fillcolor
-              onSurface: scorpion,
-              onBackground: scorpion,
-            ),
-      );
+              primary: _darkColor.meadow, // elevated button background, seperated color
+              onPrimary: _darkColor.woodsmoke, // elavated login text,  draggable container color
+              secondary: _darkColor.scorpion,
+              onSecondary: _darkColor.scorpion,
+              error: _darkColor.red,
+              onError: _darkColor.scorpion,
+              surface: _darkColor.white, // textfield  textstyle, SecureIcon(IconThemedata ile)
+              background: _darkColor.athensGray, // textfield fillcolor
+              onSurface: _darkColor.scorpion,
+              onBackground: _darkColor.scorpion,
+            ));
+  }
+}
+
+class _DarkColor {
+  final meadow = const Color(0xff19a54a);
+  final woodsmoke = const Color(0xff121515);
+  final white = const Color(0xffffffff); // elavated login text
+  final gray = const Color(0xff888888); // your email vs text
+  final athensGray = const Color(0xfff9fafb); // textfield fillcolor(xx)
+
+  final scorpion = const Color(0xff595959); // search
+  final shark = const Color(0xff1a1e1e); // textfield  textstyle
+
+  //1a1e1e  16px 400
+  final red = Colors.red;
 }
--- lib/product/theme_notifier.dart
+++ lib/product/theme_notifier.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'init/theme/app_theme_dark.dart';
 import 'init/theme/app_theme_light.dart';
 
+// import 'init/theme/app_theme_dark.dart';
+// import 'init/theme/app_theme_light.dart';
+
 enum AppThemes { light, dark }
 
 class ThemeNotifer extends ChangeNotifier {
@@ -15,25 +18,25 @@ class ThemeNotifer extends ChangeNotifier {
   AppThemes _currentThemeEnum = AppThemes.light;
   AppThemes get currentThemeEnum => _currentThemeEnum;
 
-  ThemeData _currentTheme = AppThemeLight.instance.themeDataLight;
+  ThemeData _currentTheme = LightTheme().theme;
 
   set isDark(bool f) {
     _isDarkTheme = f;
     if (_currentThemeEnum == AppThemes.light) {
-      _currentTheme = AppThemeDark.instance.themeDataDark;
+      _currentTheme = DarkTheme().theme;
     } else {
-      _currentTheme = AppThemeLight.instance.themeDataLight;
+      _currentTheme = LightTheme().theme;
     }
     notifyListeners();
   }
 
   ThemeData get currentTheme {
     if (_isDarkTheme) {
-      _currentTheme = AppThemeDark.instance.themeDataDark;
+      _currentTheme = DarkTheme().theme;
       _currentThemeEnum = AppThemes.dark;
     } else {
       // light tema seçiliyse light temada kalmasını sağlıyor
-      _currentTheme = AppThemeLight.instance.themeDataLight;
+      _currentTheme = LightTheme().theme;
       _currentThemeEnum = AppThemes.light;
     }
     return _currentTheme;
